b7c0a66911e1d6cc4a833b9436c46f5c7e3e4fdc
aayushi1139 <lalitahaj@gmail.com>
2026-04-07 15:33:39 +0530

    Delete movie app directory

--- movie app/java/com/cg/demo/repo/EmpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.demo.entities.Emp;
-
-
-public interface EmpRepository extends JpaRepository<Emp, Integer> {
-}
